cc51fe8b91cbc958e762f226d166ff84fabc680f
Bree-Coding <brendapollagba@gmail.com>
2025-04-23 01:11:25 +0200

    feat: add type-annotated functions for various operations and update README

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""A type-annotated function to concatenate two strings"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """return the sum of the two concatenated strings"""
+    return str1 + str2
---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""A type-annotated function to return the floor of a float"""
+
+
+def floor(n: float) -> int:
+    """Return the floor of a float"""
+    return int(n)
