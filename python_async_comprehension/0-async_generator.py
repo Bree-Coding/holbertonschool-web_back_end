c6d2f69f8e13ab2d5c335c441a12224e1b9223e4
Bree-Coding <brendapollagba@gmail.com>
2025-05-04 00:15:46 +0200

    fix: update type hint for async_generator function to use AsyncGenerator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """Module containing an asynchronous generator function"""
 import asyncio
 import random
-from typing import Generator
+from typing import AsyncGenerator
 
 
-async def async_generator() -> Generator[float, None, None]:
+async def async_generator() -> AsyncGenerator[float, None, None]:
     """Async generator that yields random numbers between 0 and 10."""
     for _ in range(10):
         await asyncio.sleep(1)
