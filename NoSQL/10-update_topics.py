3f968cf05c3396d065282a998d223d0557cb92e7
Bree-Coding <brendapollagba@gmail.com>
2025-05-04 00:08:00 +0200

    fix: add missing docstrings to functions in multiple files

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -3,4 +3,5 @@
 
 
 def update_topics(mongo_collection, name, topics):
+    """Update topics of a document based on name"""
     mongo_collection.update_many({ "name": name }, { "$set": { "topics": topics } })
