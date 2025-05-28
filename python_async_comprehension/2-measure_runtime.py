13bd2ac42df11bec6a38a958710786d06c544341
Bree-Coding <brendapollagba@gmail.com>
2025-05-28 11:53:32 +0200

    fix: update module docstring and improve runtime measurement in measure_runtime function

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,19 +1,17 @@
 #!/usr/bin/env python3
-"""Measure the runtime of async_comprehension."""
-
+"""
+Module 2-measure_runtime - Measure Runtime
+"""
 import asyncio
 import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    """Measure the runtime of async_comprehension."""
-    start_time = time.perf_counter()
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
-    )
-    end_time = time.perf_counter()
-    return end_time - start_time
+    """
+    Measure the runtime of async_comprehension
+    """
+    start = time.time()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    end = time.time()
+    return end - start
