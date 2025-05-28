a1a069fc21c213c91cc5fa27610055727aa12966
Bree-Coding <brendapollagba@gmail.com>
2025-05-28 11:55:17 +0200

    fix: format docstring for better readability in measure_runtime function

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
-"""Measures the total runtime of running async_comprehension four times in parallel."""
+"""Measures the total runtime of running
+async_comprehension four times in parallel."""
 import asyncio
 import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    """Executes async_comprehension 4 times concurrently and returns the total runtime in seconds."""
+    """Executes async_comprehension 4 times concurrently
+    and returns the total runtime in seconds."""
     start = time.time()
     await asyncio.gather(*(async_comprehension() for _ in range(4)))
     end = time.time()
