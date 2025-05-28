1254850bc02e7e6c4cb40ff218d26b02333d6f4a
Bree-Coding <brendapollagba@gmail.com>
2025-05-28 10:48:02 +0200

    fix: correct shebang line and update docstring in insert_school function

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,7 +1,7 @@
-#!usr/bin/env python3
+#!/usr/bin/env python3
 """Insert a document in a collection"""
 
 
 def insert_school(mongo_collection, **kwargs):
-    """Insert a document in a collection"""
+    """Insert a document node """
     return mongo_collection.insert_one(kwargs).inserted_id
